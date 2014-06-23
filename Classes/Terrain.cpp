64f0f02bf261e81a599e02fe0783340d3b10f5c8
konopka <konopka@foundation.sk>
2014-06-24 00:33:17 +0200

    Step 8: better hills algorithm

--- Classes/Terrain.cpp
+++ Classes/Terrain.cpp
@@ -37,12 +37,43 @@ bool Terrain::init()
 void Terrain::generateHills()
 {
 	Size winSize = Director::getInstance()->getWinSize();
-	float x = 0;
+
+	float minDX = 160;
+	float minDY = 60;
+	int rangeDX = 80;
+	int rangeDY = 40;
+
+	float x = -minDX;
 	float y = winSize.height / 2;
-	for (int i = 0; i < kMaxHillKeyPoints; ++i) {
+
+	float dy, ny;
+	float sign = 1; // +1 - going up, -1 - going  down
+	float paddingTop = 20;
+	float paddingBottom = 20;
+
+	for (int i = 0; i<kMaxHillKeyPoints; i++) {
 		_hillKeyPoints[i] = Vec2(x, y);
-		x += winSize.width / 2;
-		y = (rand() % (int)winSize.height);
+
+		if (i == 0) {
+			x = 0;
+			y = winSize.height / 2;
+		}
+		else {
+			x += rand() % rangeDX + minDX;
+
+			while (true) {
+				dy = rand() % rangeDY + minDY;
+				ny = y + dy*sign;
+
+				if (ny < winSize.height - paddingTop && ny > paddingBottom) {
+					break;
+				}
+			}
+
+			y = ny;
+		}
+
+		sign *= -1;
 	}
 }
 
